5d08739282073c014bbb6b9f636af12febe8fef8
zhaozhentao <qq344696734@gmail.com>
2022-04-25 15:52:36 +0800

    可以识别车牌

--- src/main/java/machine/OnAppStarted.java
+++ src/main/java/machine/OnAppStarted.java
@@ -6,7 +6,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.tensorflow.Graph;
 import org.tensorflow.Session;
-import org.tensorflow.Tensor;
 import org.tensorflow.TensorFlow;
 import org.tensorflow.op.Ops;
 import org.tensorflow.op.image.DecodeJpeg;
@@ -16,10 +15,20 @@ import org.tensorflow.types.TFloat32;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Component
 public class OnAppStarted implements ApplicationRunner {
+
+    private final String[] chars = new String[]{
+        "京", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑", "苏",
+        "浙", "皖", "闽", "赣", "鲁", "豫", "鄂", "湘", "粤", "桂", "琼",
+        "川", "贵", "云", "藏", "陕", "甘", "青", "宁", "新",
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "A", "B", "C", "D", "E", "F", "G", "H", "J", "K",
+        "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V",
+        "W", "X", "Y", "Z", ""
+    };
 
     @Override
     public void run(ApplicationArguments args) throws IOException {
@@ -53,7 +62,7 @@ public class OnAppStarted implements ApplicationRunner {
         }
     }
 
-    private void carPlateRecognize() {
+    private void carPlateRecognize() throws IOException {
         try (
             var g = new Graph();
             var s = new Session(g);
@@ -85,11 +94,24 @@ public class OnAppStarted implements ApplicationRunner {
                 .fetch("Identity_7")
                 .run();
 
-            for (Tensor t : resultTensor) {
-                System.out.println(t.shape());
-            }
-        } catch (Exception e) {
-            System.err.println(Arrays.toString(e.getStackTrace()));
+            String plate = resultTensor.stream().map(t -> {
+                float[] r = new float[(int) t.shape().asArray()[1]];
+                t.asRawTensor().data().asFloats().read(r);
+                t.close();
+                return chars[findMax(r)];
+            }).collect(Collectors.joining());
+
+            System.out.println(plate);
         }
+    }
+
+    private int findMax(float[] array) {
+        if (array == null || array.length == 0) return -1;
+
+        int largest = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[largest]) largest = i;
+        }
+        return largest;
     }
 }
