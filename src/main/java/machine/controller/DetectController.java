fa688d1108b426da838c7260466682aaad1e6e33
zhaozhentao <qq344696734@gmail.com>
2022-06-02 09:54:45 +0800

    更新java

--- src/main/java/machine/controller/DetectController.java
+++ src/main/java/machine/controller/DetectController.java
@@ -5,10 +5,7 @@ import machine.components.OcrModel;
 import machine.components.ParkingStatusModel;
 import machine.entity.ParkingStateResult;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
@@ -32,22 +29,33 @@ public class DetectController {
     @Resource
     ThreadPoolTaskExecutor executor;
 
+    private HashMap<String, Object> result(Object data) {
+        return new HashMap<>() {{
+            put("resultCode", 0);
+            put("msg", "Success");
+            put("msgParameters", "Success");
+            put("data", data);
+        }};
+    }
+
     @PostMapping("/lpr")
     public Object detect(@RequestParam("file") MultipartFile file) throws Exception {
         return executor.submit(() -> {
+            var begin = System.currentTimeMillis();
+
             var detect = detectModel.carPlateDetect(formToImage(file, 625, 625));
 
             var plate = ocrModel.carPlateRecognize(detect.plateImage);
 
-            var m = new HashMap<String, Object>();
-            m.put("left", Math.min(detect.leftTop.x, detect.leftBottom.x));
-            m.put("right", Math.max(detect.rightTop.x, detect.rightBottom.x));
-            m.put("top", Math.min(detect.leftTop.y, detect.rightTop.y));
-            m.put("bottom", Math.max(detect.leftBottom.y, detect.rightBottom.y));
-            m.put("plate", plate);
-            m.put("confidence", 0);
-            m.put("timeSpent", 0);
-            return m;
+            return result(new HashMap<String, Object>() {{
+                put("left", Math.min(detect.leftTop.x, detect.leftBottom.x));
+                put("right", Math.max(detect.rightTop.x, detect.rightBottom.x));
+                put("top", Math.min(detect.leftTop.y, detect.rightTop.y));
+                put("bottom", Math.max(detect.leftBottom.y, detect.rightBottom.y));
+                put("plate", plate);
+                put("confidence", 0);
+                put("timeSpent", System.currentTimeMillis() - begin);
+            }});
         }).get();
     }
 
@@ -56,12 +64,25 @@ public class DetectController {
         return executor.submit(() -> {
             var begin = System.currentTimeMillis();
             var status = parkingStatusModel.predict(formToImage(file, 96, 96));
-            return new ParkingStateResult(status, System.currentTimeMillis() - begin);
+            return result(new ParkingStateResult(status, System.currentTimeMillis() - begin));
         }).get();
     }
 
     @PostMapping(value = "/hpc")
     public Object hpc(@RequestParam("file") MultipartFile file) {
         return "";
+    }
+
+    @GetMapping("license_request_code")
+    public Object code() {
+        return result(new HashMap<>(2) {{
+            put("isAuthorized", true);
+            put("requestCode", "F45D3A4470825697-E4060300FFFBAB1F");
+        }});
+    }
+
+    @GetMapping("/version")
+    public Object version() {
+        return result("2.0.0.1");
     }
 }
